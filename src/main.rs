a2c68ac21b9593089618765c592377feed8fa9ba
Mnwa <mihan@panfilov.biz>
2020-03-28 19:34:32 +0300

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,20 @@ use gelf::unpacking::UnPackActor;
 fn main() {
     let dsn = env::var("SENTRY_DSN")
         .unwrap_or_else(|_e| panic!("You must to pass SENTRY_DSN variable from env"));
-    let udp_addr = env::var("UDP_ADDR").unwrap_or("0.0.0.0:8080".to_owned());
-    let tcp_addr = env::var("TCP_ADDR").unwrap_or("0.0.0.0:8081".to_owned());
-    let system_name = env::var("SYSTEM").unwrap_or("Gelf Mover".to_owned());
+    let udp_addr = env::var("UDP_ADDR").unwrap_or_else(|_| "0.0.0.0:8080".to_string());
+    let tcp_addr = env::var("TCP_ADDR").unwrap_or_else(|_| "0.0.0.0:8081".to_string());
+    let system_name = env::var("SYSTEM").unwrap_or_else(|_| "Gelf Mover".to_string());
 
     let reader_threads: usize = env::var("READER_THREADS")
-        .unwrap_or("1".to_owned())
+        .unwrap_or_else(|_| "1".to_string())
         .parse()
         .unwrap();
     let unpacker_threads: usize = env::var("UNPACKER_THREADS")
-        .unwrap_or("1".to_owned())
+        .unwrap_or_else(|_| "1".to_string())
         .parse()
         .unwrap();
     let max_parallel_chunks: usize = std::env::var("MAX_PARALLEL_CHUNKS")
-        .unwrap_or("100000".to_owned())
+        .unwrap_or_else(|_| "100000".to_string())
         .parse()
         .unwrap();
 
@@ -37,16 +37,16 @@ fn main() {
     let gelf_sentry_processor = SentryProcessorActor::new(&dsn, reader_threads);
     let _gelf_printer = GelfPrinterActor::new();
     actix::spawn(udp_acceptor::new_udp_acceptor(
-        udp_addr.to_string(),
+        udp_addr,
         gelf_sentry_processor.clone(),
         gelf_reader.clone(),
-        gelf_unpacker.clone(),
+        gelf_unpacker,
         max_parallel_chunks,
     ));
     actix::spawn(tcp_acceptor::new_tcp_acceptor(
-        tcp_addr.to_string(),
-        gelf_sentry_processor.clone(),
-        gelf_reader.clone(),
+        tcp_addr,
+        gelf_sentry_processor,
+        gelf_reader,
     ));
     system.run().unwrap();
 }
